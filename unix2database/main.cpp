1162cedbdec4b2c5b0ad62c2c7b06b33e29d3041
Scott Harvey <scott@spharvey.me>
2018-08-02 16:47:18 -0500

    All files in database and accounted for

--- unix2database/main.cpp
+++ unix2database/main.cpp
@@ -1,5 +1,5 @@
-#include <QCoreApplication>
-
+﻿#include <QCoreApplication>
+#include <QDirIterator>>
 #include <QSqlDatabase>
 #include <QSqlDriver>
 #include <QSqlQuery>
@@ -11,6 +11,10 @@
 
 int main(int argc,char **argv)
 {
+
+    QString saveName;
+
+    qDebug() << "Started";
 
     QSqlDatabase db;
     QString dbName = QString("/home/bundito/projects/fortune/fortune.db");
@@ -29,50 +33,102 @@ int main(int argc,char **argv)
 
     // OPEN fortune file
 
-    int group_id = 9;
+        int group_id = 0;
 
-    QFile fortune_file(QString("/home/bundito/projects/fortune/fortune_files/nietzsche"));
-    if (!fortune_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "Open fortune file failed.";
-        qDebug() << fortune_file.errorString();
-        exit(1);
-    }
+        QDirIterator iter("/home/bundito/projects/fortune/fortune_files/", QDir::NoDotAndDotDot | QDir::Files);
 
 
-    QString fortune;
-    QString message;
 
-    while (!fortune_file.atEnd()) {
-        QString line = fortune_file.readLine();
+        qDebug() << "Inside iterator";
 
-        if (line == QString("%\n")) {
+
+        while (iter.hasNext()) {
+            QString file = iter.next();
+            QFile fortune_file(file);
+
+        saveName = file;
+
+
+        group_id++;
+
+
+        qDebug() << fortune_file;
+
+            if (!fortune_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                qDebug() << "Open fortune file failed.";
+                qDebug() << fortune_file.errorString();
+                exit(1);
+            }
+
+
+            QString fortune;
+            QString message;
+
+            while (!fortune_file.atEnd()) {
+                QString line = fortune_file.readLine();
+
+                if (line == QString("%\n")) {
+                    QSqlQuery sql(db);
+
+                    sql.prepare(QString("INSERT INTO fortunes \
+                                        ( message, group_id ) \
+                                          VALUES (:message, :group_id) \
+                                          "));
+
+                    sql.bindValue(QString(":message"), message);
+                    sql.bindValue((":group_id"), group_id);
+
+                    bool insert = sql.exec();
+
+                    if (!insert) {
+
+                    QString err = sql.lastError().text();
+                        qDebug() << "Insert failed: " << err;
+                        exit(1);
+                    }
+
+
+                    db.commit();
+                    message = "";
+
+
+                } else { // END FOUND %
+
+                    message.append(line);
+
+            }
+
+
+            }
+
+
             QSqlQuery sql(db);
+            sql.prepare(QString("INSERT INTO groups \
+                                ( group_id, group_name) \
+                                  VALUES (:group_id, :group_name) \
+                                "));
 
-            sql.prepare(QString("INSERT INTO fortunes \
-                                ( message, group_id ) \
-                                  VALUES (:message, :group_id) \
-                                  "));
-
-            sql.bindValue(QString(":message"), message);
-            sql.bindValue((":group_id"), group_id);
+            sql.bindValue(QString(":group_id"), group_id);
+            sql.bindValue(QString(":group_name"), saveName) ;
 
             bool insert = sql.exec();
 
             if (!insert) {
 
             QString err = sql.lastError().text();
-                qDebug() << "Insert failed: " << err;
+                qDebug() << "Group insert failed: " << err;
                 exit(1);
+
             }
 
             db.commit();
-            message = "";
-
-        } else { // END FOUND %
-
-            message.append(line);
-
-        }
 
     }
+
+
+
+
+
+
+
 }
